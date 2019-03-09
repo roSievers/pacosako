18ebb3448777a739fd6b092faf64505d90a45192
Rolf Kreibaum <rolf.sievers@posteo.de>
2019-03-09 12:43:28 +0100

    Move piece data into BoardService

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,12 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { BoardComponent } from "./board/board.component";
-import { LogComponent } from "./log/log.component";
-import { ButtonsComponent } from "./buttons/buttons.component";
-import { PieceComponent } from "./piece/piece.component";
-import { LoggerService } from "./logger.service";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { LogComponent } from './log/log.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { PieceComponent } from './piece/piece.component';
 
 @NgModule({
   declarations: [
@@ -15,10 +14,10 @@ import { LoggerService } from "./logger.service";
     BoardComponent,
     LogComponent,
     ButtonsComponent,
-    PieceComponent
+    PieceComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [LoggerService],
-  bootstrap: [AppComponent]
+  providers: [],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
--- frontend/src/app/board/board.component.ts
+++ frontend/src/app/board/board.component.ts
@@ -1,51 +1,26 @@
-import { Component, OnInit } from "@angular/core";
-import { ChessPiece, PieceType, PlayerColor } from "../types";
-import { LoggerService } from "../logger.service";
+import { Component, OnInit } from '@angular/core';
+import { ChessPiece } from '../types';
+import { LoggerService } from '../logger.service';
+import { BoardService } from '../board.service';
 
 @Component({
-  selector: "app-board",
-  templateUrl: "./board.component.html",
-  styleUrls: ["./board.component.css"]
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.css'],
 })
 export class BoardComponent implements OnInit {
-  pieces: ChessPiece[] = [
-    new ChessPiece(PieceType.rock, PlayerColor.white, { x: 0, y: 0 }),
-    new ChessPiece(PieceType.knight, PlayerColor.white, { x: 1, y: 0 }),
-    new ChessPiece(PieceType.bishop, PlayerColor.white, { x: 2, y: 0 }),
-    new ChessPiece(PieceType.queen, PlayerColor.white, { x: 3, y: 0 }),
-    new ChessPiece(PieceType.king, PlayerColor.white, { x: 4, y: 0 }),
-    new ChessPiece(PieceType.bishop, PlayerColor.white, { x: 5, y: 0 }),
-    new ChessPiece(PieceType.knight, PlayerColor.white, { x: 6, y: 0 }),
-    new ChessPiece(PieceType.rock, PlayerColor.white, { x: 7, y: 0 }),
-    new ChessPiece(PieceType.pawn, PlayerColor.white, { x: 0, y: 1 }),
-    new ChessPiece(PieceType.pawn, PlayerColor.white, { x: 1, y: 1 }),
-    new ChessPiece(PieceType.pawn, PlayerColor.white, { x: 2, y: 1 }),
-    new ChessPiece(PieceType.pawn, PlayerColor.white, { x: 3, y: 1 }),
-    new ChessPiece(PieceType.pawn, PlayerColor.white, { x: 4, y: 1 }),
-    new ChessPiece(PieceType.pawn, PlayerColor.white, { x: 5, y: 1 }),
-    new ChessPiece(PieceType.pawn, PlayerColor.white, { x: 6, y: 1 }),
-    new ChessPiece(PieceType.pawn, PlayerColor.white, { x: 7, y: 1 }),
-    new ChessPiece(PieceType.rock, PlayerColor.black, { x: 0, y: 7 }),
-    new ChessPiece(PieceType.knight, PlayerColor.black, { x: 1, y: 7 }),
-    new ChessPiece(PieceType.bishop, PlayerColor.black, { x: 2, y: 7 }),
-    new ChessPiece(PieceType.queen, PlayerColor.black, { x: 3, y: 7 }),
-    new ChessPiece(PieceType.king, PlayerColor.black, { x: 4, y: 7 }),
-    new ChessPiece(PieceType.bishop, PlayerColor.black, { x: 5, y: 7 }),
-    new ChessPiece(PieceType.knight, PlayerColor.black, { x: 6, y: 7 }),
-    new ChessPiece(PieceType.rock, PlayerColor.black, { x: 7, y: 7 }),
-    new ChessPiece(PieceType.pawn, PlayerColor.black, { x: 0, y: 6 }),
-    new ChessPiece(PieceType.pawn, PlayerColor.black, { x: 1, y: 6 }),
-    new ChessPiece(PieceType.pawn, PlayerColor.black, { x: 2, y: 6 }),
-    new ChessPiece(PieceType.pawn, PlayerColor.black, { x: 3, y: 6 }),
-    new ChessPiece(PieceType.pawn, PlayerColor.black, { x: 4, y: 6 }),
-    new ChessPiece(PieceType.pawn, PlayerColor.black, { x: 5, y: 6 }),
-    new ChessPiece(PieceType.pawn, PlayerColor.black, { x: 6, y: 6 }),
-    new ChessPiece(PieceType.pawn, PlayerColor.black, { x: 7, y: 6 })
-  ];
+  pieces: ChessPiece[];
 
-  constructor(public loggingService: LoggerService) {}
+  highlight: { x: number; y: number } | null = null;
 
-  ngOnInit() {}
+  constructor(
+    public loggingService: LoggerService,
+    public boardService: BoardService,
+  ) {}
+
+  ngOnInit() {
+    this.pieces = this.boardService.piecesInInitialPosition();
+  }
 
   onPieceClick(piece: ChessPiece) {
     this.loggingService.add(`Piece clicked: ${JSON.stringify(piece)}`);
